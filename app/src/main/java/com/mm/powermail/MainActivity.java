f56a44185157c3d97b3ed1e6b90f3ddb36e07732
zhangwx <1257545592@qq.com>
2018-01-07 22:32:53 +0800

    msg2

--- app/src/main/java/com/mm/powermail/MainActivity.java
+++ app/src/main/java/com/mm/powermail/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mm.powermail;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.mm.powermail.ui.activity.NewAccountsActivity;
+import com.mm.powermail.ui.activity.SettingsActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -82,8 +86,9 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            startActivity(new Intent(this, SettingsActivity.class));
         } else if (id == R.id.nav_gallery) {
-
+            startActivity(new Intent(this, NewAccountsActivity.class));
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
